2f0f3a7323871ad8b7c6894848ba3e026f5b7fb7
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-09 21:59:39 +0300

    initial

---
+++ index.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api')
+
+
+const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
+
+const bot = new TelegramBot(TOKEN, {polling: true})
+
+bot.on('message', msg => {
+    bot.sendMessage(msg.chat.id, `Hello from SPB, bot says: "Привет, ${msg.from.first_name}"`)
+})
