29a68a91eef07461d23022a0f76491391081fb10
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-10 21:26:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 const bot = new TelegramBot(TOKEN, {polling: true})
 
 bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello from SPB, bot says: "Привет, ${msg.from.first_name}"`)
+    bot.sendMessage(msg.chat.id, `Hello, bot says: "Привет, ${msg.from.first_name}"`)
 })
