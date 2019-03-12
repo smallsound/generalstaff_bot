7aa4be6d39c61d92b3011a3db94abccde1f884d1
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-12 15:55:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,29 +1,17 @@
-const TelegramBot = require('node-telegram-bot-api')
+const TelegramBot = require('node-telegram-bot-api') 
 
 const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.keyboard([
-  [{':one: go page 1': {go: 'page1'}}],
-  [{':two: go page 2': {go: 'page2'}}],
-  [{':three: go page 3': {go: 'page3'}}]
-])
+bot.onText(/\/start/, (msg) => {
+    
+bot.sendMessage(msg.chat.id, `Hello from SPB, bot says: "Привет, ${msg.from.first_name}"`, {
+"reply_markup": {
+    "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["I'm robot"]]
+    }
+});
+    
+});
 
-bot.command('page1').invoke(function (ctx) {
-  return ctx.sendMessage('This is page 1')
-})
 
-bot.command('page2').invoke(function (ctx) {
-  return ctx.sendMessage('This is page 2')
-}).keyboard([
-  [{':one: go page 1': {go: 'page1'}}],
-  [{':three: go page 3': {go: 'page3'}}]
-])
-
-bot.command('page3').invoke(function (ctx) {
-  ctx.keyboard([
-    [{':one: go page 1': {go: 'page1'}}]
-    [{':two: go page 2': {go: 'page2'}}]
-  ])
-})
