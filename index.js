baea359c77b4f3fe2ef96c55f96223dde920470f
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-12 16:09:12 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ bot.sendMessage(msg.chat.id, `Добро пожаловать ${msg.from.first_n
 });
 
 
+bot.on('message', (msg) => {
+var Hi = "hi";
+if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
+    bot.sendMessage(msg.chat.id, "Hello dear user");
+}
+var bye = "bye";
+if (msg.text.toString().toLowerCase().includes(bye)) {
+    bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+}    
+var robot = "I'm robot";
+if (msg.text.indexOf(robot) === 0) {
+    bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
+}
+});
+
+
