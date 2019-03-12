56e8a7e78c5fa2d4596b093ec541a6d465936163
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-12 15:28:44 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,28 +4,26 @@ const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
+bot.keyboard([
+  [{':one: go page 1': {go: 'page1'}}],
+  [{':two: go page 2': {go: 'page2'}}],
+  [{':three: go page 3': {go: 'page3'}}]
+])
 
-bot.keyboard('footer', [{':arrow_backward:': {go: 'start'}}])
+bot.command('page1').invoke(function (ctx) {
+  return ctx.sendMessage('This is page 1')
+})
 
-bot.command('start', function (ctx) {
-  ctx.sendMessage('Hello there')
+bot.command('page2').invoke(function (ctx) {
+  return ctx.sendMessage('This is page 2')
 }).keyboard([
-  [{'Page 1': {go: 'page1'}}],
-  [{'Page 2': {go: 'page2'}}]
+  [{':one: go page 1': {go: 'page1'}}],
+  [{':three: go page 3': {go: 'page3'}}]
 ])
 
-bot.command('page1', function () {
-  ctx.sendMessage('This is page 1')
+bot.command('page3').invoke(function (ctx) {
+  ctx.keyboard([
+    [{':one: go page 1': {go: 'page1'}}]
+    [{':two: go page 2': {go: 'page2'}}]
+  ])
 })
-.keyboard([
-  [{'Page 2': {go: 'page2'}}],
-  'footer'
-])
-
-bot.command('page2', function () {
-  ctx.sendMessage('This is page 1')
-})
-.keyboard([
-  [{'Page 1': {go: 'page1'}}],
-  'footer'
-])
