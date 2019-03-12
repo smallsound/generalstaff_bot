1f9e68851be9f8292c7cd1721cc62f3626ac18a8
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-12 16:17:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,26 +8,14 @@ bot.onText(/\/start/, (msg) => {
     
 bot.sendMessage(msg.chat.id, `Добро пожаловать ${msg.from.first_name}✌`, {
 "reply_markup": {
-    "keyboard": [["Sample text"],   ["Keyboard"], ["I'm robot"]]
+    "keyboard": [["1 text"],   ["2 text"], ["3 text"]]
     }
 });
     
 });
 
 
-bot.on('message', (msg) => {
-var Hi = "hi";
-if (msg.text.toString().toLowerCase().indexOf(Hi) === 0) {
-    bot.sendMessage(msg.chat.id, "Hello dear user");
-}
-var bye = "bye";
-if (msg.text.toString().toLowerCase().includes(bye)) {
-    bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
-}    
-var robot = "I'm robot";
+var robot = "1 text";
 if (msg.text.indexOf(robot) === 0) {
-    bot.sendMessage(msg.chat.id, "Yes I'm robot but not in that way!");
-}
-});
-
+    bot.sendMessage(msg.chat.id, "ответ 1");
 
