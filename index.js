e983fe4e4c40f8c84ccc82f28fb0bd7f4902c4e8
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-12 13:52:14 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const TelegramBot = require('node-telegram-bot-api')
 
-
 const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
 
-bot.on('message', msg => {
-    bot.sendMessage(msg.chat.id, `Hello, bot says: "Привет, ${msg.from.first_name}"`)
-})
+
+bot.onText(/\/start/, (msg) => {
+    
+bot.sendMessage(msg.chat.id, "Welcome", {
+"reply_markup": {
+    "keyboard": [["Sample text", "Second sample"],   ["Keyboard"], ["I'm robot"]]
+    }
+});
+    
+});
