a722f1e59854d768e725badecba57eefc708abc9
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-12 23:11:36 +0300

    Update index.js

--- index.js
+++ index.js
@@ -14,4 +14,18 @@ bot.sendMessage(msg.chat.id, `Добро пожаловать ${msg.from.first_n
     
 
 
+
+
+var options = {
+  reply_markup: JSON.stringify({
+    inline_keyboard: [
+      [{ text: 'Кнопка 1', callback_data: '1' }],
+      [{ text: 'Кнопка 2', callback_data: 'data 2' }],
+      [{ text: 'Кнопка 3', callback_data: 'text 3' }]
+    ]
+  })
+};
+
+bot.onText(/\/start_test/, function (msg, match) {
+  bot.sendMessage(msg.chat.id, 'Выберите любую кнопку:', options);
 });
