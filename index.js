5b2dd2bee1feaf9e2385adbc825042467a7e4fa9
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-13 21:02:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api')
 
 const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 
-const bot = new TelegramBot(TOKEN, {polling: true})
+const bot = new TelegramBot(TOKEN, {polling: true});
     
 
 bot.onText (/ height /, (msg) => { 
