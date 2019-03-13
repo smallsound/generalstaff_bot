13ad3e8c16ec8f7b7f21a282b94ad3d5d1db5138
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-13 20:44:13 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,15 @@ const TelegramBot = require('node-telegram-bot-api')
 const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
-
-bot.onText(/\/start/, (msg) => {
     
-bot.sendMessage(msg.chat.id, `Добро пожаловать ${msg.from.first_name}✌`, {
-"reply_markup": {
-    "keyboard": [
-        [{ text: 'Кнопка 1', callback_data: '1' }],   ["2 text"], ["3 text"]]
-    }
-});
+
+bot.onText (/ height /, (msg) => { 
+    getHeight () 
+        .then (height => bot.sendMessage (msg.chat.id, height)) 
+        .catch (error => bot.sendMessage (msg.chat. id, 'Not found')); 
+});    
+    
+                      
+ 
 
 });
