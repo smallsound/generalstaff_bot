c91a9fbccf657b53ed48a83886a98136d7eeb0d9
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-13 22:34:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,21 +4,15 @@ const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 
 const bot = new TelegramBot(TOKEN, {polling: true});
     
-
 bot.onText(/\/start/, (msg) => {
     
+bot.sendMessage(msg.chat.id, `Добро пожаловать 🚙 ${msg.from.first_name}✌`, {
+"reply_markup": {
+    "keyboard": [[" 💦 МОЙКА 💦 "],  [" 🏁 АЗС 🏁 "], [" 🛠 СТО-РЕМОНТ 🛠 "]]
+    }
+});
+    
 
-
-var menuButtons = {
-    parse_mode: 'markdown',
-    disable_web_page_preview: false,
-    reply_markup: JSON.stringify({
-        inline_keyboard:
-        [
-            [{text: 'Сегодня', callback_data: 'today'}]
-        ]
-    })
-};
 
 
 });
