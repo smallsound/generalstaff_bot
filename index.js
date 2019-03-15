f8773382f3ae052b088d3c1641c86d4844ccd60c
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-15 21:27:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
       
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-	bot.send_message(
-		message.chat.id,
-		'Добро пожаловать. ✌
-	        ',
-		reply_markup=keyboard())
+@bot.message_handler(commands = ['switch'])
+def switch(message):
+    markup = types.InlineKeyboardMarkup()
+    switch_button = types.InlineKeyboardButton(text='Try', switch_inline_query="Telegram")
+    markup.add(switch_button)
+    bot.send_message(message.chat.id, "Выбрать чат", reply_markup = markup)
 });
     
 
