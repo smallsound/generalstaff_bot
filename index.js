beebaceab09a5c2fa7768d5fdbf373fec71d7522
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-15 20:23:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const TelegramBot = require('node-telegram-bot-api')
 
 const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 
-const bot = new TelegramBot(TOKEN, {polling: true});
+const bot = new TelegramBot(TOKEN, {polling: true})
       
 bot.sendMessage(chatId, 'Выберите какая валюта вас интересует', {
     reply_markup: {
