6cca10a3552405bf7f676c68e5e41970fdce445f
smallsound <48187395+smallsound@users.noreply.github.com>
2019-03-15 21:21:59 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,50 +4,10 @@ const TOKEN = '724656935:AAHtNlNoa8Da6UO7sQzh345UT1gcKPcD01Y'
 
 const bot = new TelegramBot(TOKEN, {polling: true})
       
-var l1 = "left";
-var r1 = "right";
-
-var g1 = "Good";
-var b1 = "Bad";
-
-  bot.onText(/\/step2/, (msg) => {
-        bot.sendMessage(msg.chat.id, "Who is playing?\n And you must write <<И>>", {});
-        bot.on('text', (msg) => {
-        if (msg.text.toLowerCase().includes("и")) {
-          bot.sendMessage(msg.chat.id, "This is left or right?", {
-            "reply_markup": {
-              "keyboard": [[l1 , r1]]
-            }
-          });
-          bot.on('text', (msg) => {
-            if (msg.text === l1) {
-              bot.sendMessage(msg.chat.id, "Good or bad?", {
-                "reply_markup": {
-                  "keyboard": [[g1 , b1]]
-                }
-              });
-              bot.on('text', (msg) => {
-                if (msg.text === g1) {
-                  bot.sendMessage(msg.chat.id, "So, you can go \n /step3", {});
-                } else if (msg.text === b1) {
-                  bot.sendMessage(msg.chat.id, "So, you can go \n /step3", {});
-                }
-              });
-            } else if (msg.text === r1) {
-              bot.sendMessage(msg.chat.id, "Good or bad?", {
-                "reply_markup": {
-                  "keyboard": [[g1 , b1]]
-                }
-              });
-              bot.on('text', (msg) => {
-                if (msg.text === g1) {
-                  bot.sendMessage(msg.chat.id, "So, you can go \n /step3", {});
-                } else if (msg.text === b1) {
-                  bot.sendMessage(msg.chat.id, "So, you can go \n /step3", {});
-                }
-              });
-            }
-          });
-          } else {}
-      });
-    });
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+	bot.send_message(
+		message.chat.id,
+		'''Добро пожаловать. ✌
+		''',
+		reply_markup=keyboard())
